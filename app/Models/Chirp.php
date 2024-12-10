10d5ea34c25982f0b5ae3913e0f2919bbbe5e039
Deane Benjie B. Santos Jr. <dbjbsantos02094@usep.edu.ph>
2024-12-10 13:24:46 +0800

    For Deployment Chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -9,16 +10,18 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Chirp extends Model
 {
     use HasFactory;
-
+ 
     protected $fillable = [
         'message',
     ];
-
+ 
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
+    ];
+ 
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
-
 }
-
 
